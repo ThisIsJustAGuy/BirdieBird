cbc8802df5982cbd0fd0db01c91a5c77f67f426d
ThisIsJustAGuy <bbalint0404@gmail.com>
2020-10-02 15:57:52 +0200

    :sparkles: Adds jumping and falling

--- app.js
+++ app.js
@@ -5,47 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const score = document.querySelector("#score");
   // console.log(bird.style);
 
-  let position = bird.style.bottom;
-  let gravity = 0.9;
+  let position = 200;
   let points = 0;
+  let height = window.innerHeight + "px";
 
   let isGameOver = false;
+  let isJumping = false;
 
   function control(event) {
     // console.log(event);
     if (event.keyCode === 38) {
-        jump();
+      isJumping = true;
+      jump();
     }
   }
 
   function jump() {
-    console.log("jump");
     let count = 0;
-    // let jumpId = setInterval(() => {
-    //     if (count == 15) {
-    //         clearInterval(jumpId);
-    //         let downTimerId = setInterval(() => {
-    //             position -= 5;
-    //             count--;
-    //             position = position * gravity;
-    //             bird.style.bottom = position + 'px';
-    //         }, 20);
-    //     }
-    //     count++;
-    //     position += 10;
-    //     position = position * gravity;
-    //     bird.style.bottom = position + 'px';
-    //     // console.log(position);
-    // }, 20);
     let jumpID = setInterval(() => {
       if (count == 15) {
         clearInterval(jumpID);
+        isJumping = false;
       }
-      position += 10;
-      position *= gravity;
+      position += 5;
       bird.style.bottom = position + "px";
       count++;
-    }, 20)
+    }, 20);
   }
 
   function generatePipe() {
@@ -53,11 +38,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let height = Math.random() * 400;
     const pipe = document.createElement("div");
     const pipe2 = document.createElement("div");
+    grid.appendChild(pipe);
+    grid.appendChild(pipe2);
     if (!isGameOver) {
       pipe.classList.add("pipe");
       pipe2.classList.add("pipe");
-      grid.appendChild(pipe);
-      grid.appendChild(pipe2);
       pipe.style.height = height + "px";
       pipe2.style.height = window.innerHeight - height - 200 + "px";
     }
@@ -75,24 +60,30 @@ document.addEventListener("DOMContentLoaded", () => {
       pipe2.style.left = pipePosition + "px";
       pipe2.style.top = 0 + "px";
       if (pipePosition < 0) {
-        console.log("removing");
         grid.removeChild(pipe);
         grid.removeChild(pipe2);
-        console.log("removed");
         score.innerHTML = ++points;
       }
     }, 30);
     if (!isGameOver) {
       setTimeout(generatePipe, 1500);
-      setInterval(lowerBird, 1000);
     }
   }
-  function lowerBird() {
-    bird.style.marginBottom = bird.style.marginBottom - 1 + "px";
-    if (bird.style.marginBottom <= 0) {
-      isGameOver = true;
-    }
+  function fall() {
+      let fallID = setInterval(() => {
+        if (!isJumping) {
+        position -= 5;
+        bird.style.bottom = position + "px";
+        if (
+          bird.style.bottom === "0px" ||
+          bird.style.bottom === height
+        ) {
+          isGameOver = true;
+        }
+      }
+      }, 20);
   }
   generatePipe();
+  fall();
   document.addEventListener("keydown", control);
 });
