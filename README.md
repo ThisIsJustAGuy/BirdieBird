a34c2d87e563e7af0123bb1ff01cffcea93e2a87
ThisIsJustAGuy <bbalint0404@gmail.com>
2020-10-04 13:36:10 +0200

    :lipstick: Adds Flappy Bird textures

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # BirdieBird
 
 Flappy Bird klón HTML, CSS és JavaScript használatával
-
-A Flappy Bird textúrák nem voltak birtokomban, ezért a chrome offlie dínós játékáét használtam.
--- app.js
+++ app.js
@@ -2,40 +2,46 @@ document.addEventListener("DOMContentLoaded", () => {
   const bird = document.querySelector(".bird");
   const grid = document.querySelector("#grid");
   const alert = document.querySelector("#alert");
-  const scroe = document.querySelector("#score");
+  const score = document.querySelector("#score");
   const background = document.querySelector("#background");
-  // console.log(bird.style);
+  const container = document.querySelector("#container");
 
+  let startTime = 0;
+  let endTime = 0
+  let timeDiff = 0;
   let position = 300;
   let points = 0;
-
   let isGameOver = false;
   let isJumping = false;
 
   function control(event) {
-    // console.log(event);
     if (event.keyCode === 32) {
-      isJumping = true;
-      jump();
+      endTime = new Date();
+      timeDiff = endTime - startTime;
+      if (timeDiff > 80) {
+        isJumping = true;
+        jump();
+      }
+      startTime = new Date();
     }
   }
 
   function jump() {
     let count = 0;
     let jumpID = setInterval(() => {
-      if (count == 15) {
+      if (count == 30) {
         clearInterval(jumpID);
         isJumping = false;
       }
-      position += 5;
+      position += 2.5;
       bird.style.bottom = position + "px";
       count++;
-    }, 20);
+    }, 10);
   }
 
   function generatePipe() {
-    let pipePosition = 1200;
-    let height = Math.random() * 400;
+    let pipePosition = 1300;
+    let height = Math.random() * (document.documentElement.clientHeight - 200);
     let height2 = document.documentElement.clientHeight - height - 200;
     const pipe = document.createElement("div");
     const pipe2 = document.createElement("div");
@@ -65,14 +71,14 @@ document.addEventListener("DOMContentLoaded", () => {
         grid.removeChild(pipe2);
         score.innerHTML = ++points;
       }
-      if (
-        pipePosition >= -60 &&
-        pipePosition <= 60 &&
-        (parseFloat(bird.style.bottom) <= height ||
-          parseFloat(bird.style.bottom) >= height + 140)
-      ) {
-        isGameOver = true;
-      }
+      // if (
+      //   pipePosition >= -60 &&
+      //   pipePosition <= 60 &&
+      //   (parseFloat(bird.style.bottom) <= height ||
+      //     parseFloat(bird.style.bottom) >= height + 155)
+      // ) {
+      //   isGameOver = true;
+      // }
     }, 30);
     if (!isGameOver) {
       setTimeout(generatePipe, 1500);
@@ -82,26 +88,27 @@ document.addEventListener("DOMContentLoaded", () => {
   function fall() {
     setInterval(() => {
       if (!isJumping) {
-        position -= 5;
+        position -= 2.5;
         bird.style.bottom = position + "px";
         if (
           parseInt(bird.style.bottom) <= 0 ||
-          parseInt(bird.style.bottom) + 60 >=
+          parseInt(bird.style.bottom) + 45 >=
             document.documentElement.clientHeight
         ) {
           isGameOver = true;
         }
       }
-    }, 20);
+    }, 10);
   }
 
-  function gameOver(){
+  function gameOver() {
     alert.innerHTML = `Game Over <br> Final Score: ${points}`;
-    scroe.innerHTML = "";
+    score.innerHTML = "";
     background.style.display = "none";
+    container.style.display = "block";
   }
 
   generatePipe();
   fall();
-  document.addEventListener("keypress", control);
+  document.addEventListener("keydown", control);
 });
